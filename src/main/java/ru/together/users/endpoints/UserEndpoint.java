a70b79820fcd978ab0287c87e59892185c6dcd5b
Mikhail Stepanov <mikhail.stepanovv@yandex.ru>
2020-02-08 05:09:42 +0300

    user verify methods + some fixes

--- src/main/java/ru/together/users/endpoints/UserEndpoint.java
+++ src/main/java/ru/together/users/endpoints/UserEndpoint.java
@@ -1,10 +1,7 @@
 package ru.together.users.endpoints;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.together.users.interfaces.IUserService;
 import ru.together.users.models.*;
 import ru.together.users.services.UserService;
@@ -18,36 +15,42 @@ public class UserEndpoint implements IUserService {
     UserService userService;
 
     @Override
+    @CrossOrigin
     @RequestMapping(value = USER_GET, method = {RequestMethod.POST})
     public GetUserResponse get(@RequestBody GetUserRequest request) {
         return userService.get(request);
     }
 
     @Override
+    @CrossOrigin
     @RequestMapping(value = USER_LIST, method = {RequestMethod.GET})
     public List<UserModel> list() {
         return userService.list();
     }
 
     @Override
+    @CrossOrigin
     @RequestMapping(value = USER_UPDATE, method = {RequestMethod.POST})
     public UpdateUserResponse update(@RequestBody UpdateUserRequest request) {
         return userService.update(request);
     }
 
     @Override
+    @CrossOrigin
     @RequestMapping(value = USER_VERIFY, method = {RequestMethod.POST})
     public UpdateUserResponse verify(UpdateUserRequest request) {
         return userService.verify(request);
     }
 
     @Override
+    @CrossOrigin
     @RequestMapping(value = USER_EMAIL, method = {RequestMethod.POST})
     public ResendEmailResponse resendEmail(ResendEmailRequest request) {
         return userService.resendEmail(request);
     }
 
     @Override
+    @CrossOrigin
     @RequestMapping(value = USER_REMOVE, method = {RequestMethod.POST})
     public RemoveUserResponse remove(@RequestBody RemoveUserRequest request) {
         return userService.remove(request);
