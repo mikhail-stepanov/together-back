43a24579929c6f93200e90c99544e82144606784
Mikhail Stepanov <mikhail.stepanovv@yandex.ru>
2020-04-27 22:41:04 +0300

    ssl

--- src/main/java/ru/together/TogetherApplication.java
+++ src/main/java/ru/together/TogetherApplication.java
@@ -5,12 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication(scanBasePackages = "ru.together")
-@PropertySource("classpath:application.properties")
 public class TogetherApplication {
 
     public static void main(String[] args) {
         SpringApplication application = new SpringApplication(TogetherApplication.class);
-        application.setAdditionalProfiles("ssl");
         application.run(args);
     }
 
